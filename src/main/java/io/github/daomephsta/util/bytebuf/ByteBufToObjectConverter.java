2eee3b6ab4b86f06f1941d16b91725fc56e0f270
Daomephsta <dtnilfisk@gmail.com>
2019-05-28 10:09:47 +1200

    Remove unused ByteBuffer utilities

--- src/main/java/io/github/daomephsta/util/bytebuf/ByteBufToObjectConverter.java
+++
@@ -1,8 +0,0 @@
-package io.github.daomephsta.util.bytebuf;
-
-import net.minecraft.util.PacketByteBuf;
-
-public interface ByteBufToObjectConverter
-{
-	public <T> T readFromByteBuffer(PacketByteBuf buffer, Class<T> target);
-}
--- src/main/java/io/github/daomephsta/util/bytebuf/ObjectToByteBufConverter.java
+++
@@ -1,8 +0,0 @@
-package io.github.daomephsta.util.bytebuf;
-
-import net.minecraft.util.PacketByteBuf;
-
-public interface ObjectToByteBufConverter
-{
-	public <T> void writeToByteBuffer(PacketByteBuf buffer, T object, Class<T> clazz);
-}
