62b68c5329bf71c7e845486f81d5dec4884870ee
Daomephsta <dtnilfisk@gmail.com>
2019-11-02 10:20:30 +1300

    Encapsulate GuideDefinitition

--- src/main/java/io/github/daomephsta/inscribe/client/guide/Guide.java
+++ src/main/java/io/github/daomephsta/inscribe/client/guide/Guide.java
@@ -1,10 +1,10 @@
 package io.github.daomephsta.inscribe.client.guide;
 
 import java.util.Collection;
-import java.util.Collections;
 
+import io.github.daomephsta.inscribe.client.guide.xmlformat.definition.GuideAccessMethod;
 import io.github.daomephsta.inscribe.client.guide.xmlformat.definition.GuideDefinition;
-import io.github.daomephsta.inscribe.client.guide.xmlformat.definition.NoGuideAccessMethod;
+import io.github.daomephsta.inscribe.client.guide.xmlformat.definition.TableOfContents;
 import io.github.daomephsta.inscribe.client.guide.xmlformat.entry.XmlEntry;
 import io.github.daomephsta.inscribe.client.guide.xmlformat.theme.Theme;
 import io.github.daomephsta.inscribe.common.Inscribe;
@@ -15,23 +15,43 @@ public class Guide
 	public static final Identifier INVALID_GUIDE_ID = new Identifier(Inscribe.MOD_ID, "invalid");
 	private final GuideDefinition definition;
 	private final Collection<XmlEntry> entries;
-	
+
 	public Guide(GuideDefinition definition, Collection<XmlEntry> entries)
 	{
 		this.definition = definition;
 		this.entries = entries;
 	}
-	
-	public GuideDefinition getDefinition()
-	{
-		return definition;
-	}
-	
+
 	public Identifier getIdentifier()
 	{
 		return definition.getGuideId();
 	}
-	
+
+	public Identifier getGuideId()
+	{
+		return definition.getGuideId();
+	}
+
+	public String getTranslationKey()
+	{
+		return definition.getTranslationKey();
+	}
+
+	public TableOfContents getMainTableOfContents()
+	{
+		return definition.getMainTableOfContents();
+	}
+
+	public GuideAccessMethod getAccessMethod()
+	{
+		return definition.getAccessMethod();
+	}
+
+	public Theme getTheme()
+	{
+		return definition.getTheme();
+	}
+
 	public boolean isValid()
 	{
 		return definition.getGuideId().equals(INVALID_GUIDE_ID);
