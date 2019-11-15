5c212f6ccbec89debd5687eb22556ad03e16c218
Daomephsta <dtnilfisk@gmail.com>
2019-11-15 14:20:00 +1300

    Implement complex mixins in separate classes

--- src/main/java/io/github/daomephsta/inscribe/client/mixin/MixinReloadableResourceManagerImpl.java
+++ src/main/java/io/github/daomephsta/inscribe/client/mixin/MixinReloadableResourceManagerImpl.java
@@ -4,16 +4,13 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import io.github.daomephsta.inscribe.client.guide.GuideManager;
-import net.minecraft.client.render.model.BakedModelManager;
+import io.github.daomephsta.inscribe.client.mixinimpl.MixinImplReloadableResourceManagerImpl;
 import net.minecraft.resource.*;
 import net.minecraft.util.Void;
 
@@ -25,43 +22,9 @@ public abstract class MixinReloadableResourceManagerImpl implements ReloadableRe
 	@Shadow
 	private List<ResourceReloadListener> initialListeners;
 
-	private static final Logger INSCRIBE_LOGGER = LogManager.getLogger();
-
 	@Inject(method = "beginReloadInner(Ljava/util/concurrent/Executor;Ljava/util/concurrent/Executor;Ljava/util/List;Ljava/util/concurrent/CompletableFuture;)Lnet/minecraft/resource/ResourceReloadMonitor;", at = @At("HEAD"))
 	public void inscribe_beginReloadInner(Executor loadExecutor, Executor applyExecutor, List<ResourceReloadListener> listeners, CompletableFuture<Void> future, CallbackInfoReturnable<CompletableFuture<Void>> info)
 	{
-		injectGuideManagerAsListener();
-	}
-
-	public void injectGuideManagerAsListener()
-	{
-		boolean successInitialListeners = injectListenerBefore(initialListeners, GuideManager.INSTANCE, BakedModelManager.class);
-		boolean successListeners = injectListenerBefore(listeners, GuideManager.INSTANCE, BakedModelManager.class);
-		if (successInitialListeners || successListeners)
-			INSCRIBE_LOGGER.info("[Inscribe] Registered Guide Manager as a resource reload listener");
-	}
-
-	private boolean injectListenerBefore(List<ResourceReloadListener> listeners, ResourceReloadListener listener, Class<? extends ResourceReloadListener> targetClass)
-	{
-		int targetIndex = -1;
-		for (int i = 0; i < listeners.size(); i++)
-		{
-			if (listeners.get(i) == listener)
-				listeners.remove(i);
-			else if (targetClass.isInstance(listeners.get(i)))
-			{
-				if (targetIndex < 0)
-					targetIndex = i;
-				else
-					throw new IllegalStateException("[Inscribe] Did not expect multiple instances of " + targetClass.getName() + " in resource reload listener list");
-			}
-		}
-		if (targetIndex < 0)
-			return false;
-		else
-		{
-			listeners.add(targetIndex, listener);
-			return true;
-		}
+	    MixinImplReloadableResourceManagerImpl.injectGuideManagerAsListener(initialListeners, listeners);
 	}
 }
---
+++ src/main/java/io/github/daomephsta/inscribe/client/mixinimpl/MixinImplReloadableResourceManagerImpl.java
@@ -0,0 +1,45 @@
+package io.github.daomephsta.inscribe.client.mixinimpl;
+
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import io.github.daomephsta.inscribe.client.guide.GuideManager;
+import net.minecraft.client.render.model.BakedModelManager;
+import net.minecraft.resource.*;
+
+public abstract class MixinImplReloadableResourceManagerImpl
+{
+	private static final Logger INSCRIBE_LOGGER = LogManager.getLogger();
+
+	public static void injectGuideManagerAsListener(List<ResourceReloadListener> initialListeners, List<ResourceReloadListener> listeners)
+	{
+		boolean successInitialListeners = injectListenerBefore(initialListeners, GuideManager.INSTANCE, BakedModelManager.class);
+		boolean successListeners = injectListenerBefore(listeners, GuideManager.INSTANCE, BakedModelManager.class);
+		if (successInitialListeners || successListeners)
+			INSCRIBE_LOGGER.info("[Inscribe] Registered Guide Manager as a resource reload listener");
+	}
+
+	private static boolean injectListenerBefore(List<ResourceReloadListener> listeners, ResourceReloadListener listener, Class<? extends ResourceReloadListener> targetClass)
+	{
+		int targetIndex = -1;
+		for (int i = 0; i < listeners.size(); i++)
+		{
+			if (listeners.get(i) == listener)
+				listeners.remove(i);
+			else if (targetClass.isInstance(listeners.get(i)))
+			{
+				if (targetIndex < 0)
+					targetIndex = i;
+				else
+					throw new IllegalStateException("[Inscribe] Did not expect multiple instances of " + targetClass.getName() + " in resource reload listener list");
+			}
+		}
+		if (targetIndex < 0)
+			return false;
+		else
+		{
+			listeners.add(targetIndex, listener);
+			return true;
+		}
+	}
+}
