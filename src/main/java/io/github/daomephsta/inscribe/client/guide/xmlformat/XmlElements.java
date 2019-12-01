369345c7e36f75676213ae142489c088f2219e8b
Daomephsta <dtnilfisk@gmail.com>
2019-12-01 22:43:26 +1300

    Implement XmlElements.asVector3f(Element, String) & XmlElements#asVector3f(Element, String, Vector3f)

--- src/main/java/io/github/daomephsta/inscribe/client/guide/xmlformat/XmlElements.java
+++ src/main/java/io/github/daomephsta/inscribe/client/guide/xmlformat/XmlElements.java
@@ -7,6 +7,7 @@ import org.jdom2.Element;
 
 import com.google.common.base.Splitter;
 
+import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.InvalidIdentifierException;
 
@@ -87,6 +88,51 @@ public class XmlElements
 	{
 		return getChild(xml, childName).getText();
 	}
+
+    /**
+     * Gets an xml element as a Vector3f
+     * @param xml the parent element
+     * @param childName the name of the child element
+     * @throws
+     * InscribeSyntaxException if the child element does not exist or could not be parsed as a Vector3f
+     * @return the value of the element as a Vector3f
+     */
+	public static Vector3f asVector3f(Element xml, String childName) throws InscribeSyntaxException
+	{
+	    Element child = xml.getChild(childName);
+	    XmlAttributes.requireAttributes(xml, "x", "y", "z");
+	    return new Vector3f
+        (
+            XmlAttributes.asFloat(child, "x", 0.0F),
+            XmlAttributes.asFloat(child, "y", 0.0F),
+            XmlAttributes.asFloat(child, "z", 0.0F)
+        );
+	}
+
+    /**
+     * Gets an xml element as a Vector3f
+     * @param xml the parent element
+     * @param childName the name of the child element
+     * @throws
+     * InscribeSyntaxException if the child element could not be parsed as a Vector3f
+     * @return the value of the element as a Vector3f or {@code fallback} if it does not exist
+     */
+    public static Vector3f asVector3f(Element xml, String childName, Vector3f fallback) throws InscribeSyntaxException
+    {
+        Element child = xml.getChild(childName);
+        if (child != null)
+        {
+            XmlAttributes.requireAttributes(child, "x", "y", "z");
+            return new Vector3f
+            (
+                XmlAttributes.asFloat(child, "x", fallback.getX()),
+                XmlAttributes.asFloat(child, "y", fallback.getY()),
+                XmlAttributes.asFloat(child, "z", fallback.getZ())
+            );
+        }
+        else
+            return fallback;
+    }
 
 	/**
 	 * @throws InscribeSyntaxException
