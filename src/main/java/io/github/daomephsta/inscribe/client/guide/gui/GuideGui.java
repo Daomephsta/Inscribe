efc837746349386cc5ca13c1de1572fcf66eef9d
Daomephsta <dtnilfisk@gmail.com>
2019-12-04 20:30:40 +1300

    Document GuideGui interface

--- src/main/java/io/github/daomephsta/inscribe/client/guide/gui/GuideGui.java
+++ src/main/java/io/github/daomephsta/inscribe/client/guide/gui/GuideGui.java
@@ -1,7 +1,14 @@
 package io.github.daomephsta.inscribe.client.guide.gui;
 
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.Identifier;
 
+/**
+ * Implemented by an {@link Screen} to designate it as a guide GUI.
+ * Any mod implementing their own GUI for their Inscribe guide must implement this interface.
+ * @author Daomephsta
+ *
+ */
 public interface GuideGui
 {
     public void openEntry(Identifier entryId);
