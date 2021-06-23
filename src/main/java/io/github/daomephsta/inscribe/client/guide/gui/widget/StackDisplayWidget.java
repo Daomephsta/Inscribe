7239ac452978a3179105d59b794bd52ae2920b9c
Daomephsta <dtnilfisk@gmail.com>
2021-06-23 18:15:09 +1200

    Improve stack display widget rendering

--- src/main/java/io/github/daomephsta/inscribe/client/guide/gui/widget/StackDisplayWidget.java
+++ src/main/java/io/github/daomephsta/inscribe/client/guide/gui/widget/StackDisplayWidget.java
@@ -3,6 +3,7 @@ package io.github.daomephsta.inscribe.client.guide.gui.widget;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector4f;
 import net.minecraft.item.ItemStack;
 
 public class StackDisplayWidget extends GuideWidget
@@ -19,7 +20,12 @@ public class StackDisplayWidget extends GuideWidget
     {
         MinecraftClient client = MinecraftClient.getInstance();
         ItemStack stack = stacks[(int) ((System.currentTimeMillis() / 2000) % stacks.length)];
-        client.getItemRenderer().renderGuiItem(stack, x(), y());
+        matrices.push();
+        Vector4f a = new Vector4f(x(), y(), 1.0F, 1.0F);
+        a.transform(matrices.peek().getModel());
+        matrices.translate(a.getX(), a.getY(), client.getItemRenderer().zOffset);
+        client.getItemRenderer().renderGuiItemIcon(stack, 0, 0);
+        matrices.pop();
     }
 
     @Override
